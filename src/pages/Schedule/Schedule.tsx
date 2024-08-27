c0ebd64c6eb02fe3aec1076332be31e875d0b6c1
Far4Ru <faty422@mail.ru>
2024-08-27 12:45:54 +0300

    Updated data

--- src/pages/Schedule/Schedule.tsx
+++ src/pages/Schedule/Schedule.tsx
@@ -2,10 +2,21 @@ import "../../style/Schedule.css"
 import React, { useState, useEffect }  from "react";
 import Card from "./components/Card";
 
+export interface Lecture {
+  weekType: string,
+  time: string,
+  name: string,
+  type: string,
+  lecturer: string,
+  classroom: string
+}
+
 interface DataConfig {
   name: string,
+  firstDay: string,
   schedule: {
     title: string
+    lectures: Lecture[]
   }[]
 }
 
@@ -15,15 +26,18 @@ export enum WeekType {
 }
 
 const Schedule: React.FC = () => { 
-  const [data, setData] = useState<DataConfig>({ name: '', schedule: []});
+  const [data, setData] = useState<DataConfig>({ name: '', firstDay: '01.01.1970', schedule: []});
   const [weekType, setWeekType] = useState<WeekType>(WeekType.ODD);
+  const [firstDay, setFirstDay] = useState<Date>(new Date());
+  const [today, setToday] = useState<Date>(new Date());
 
   useEffect(
     () => {
       getData().then((res)=>{
         setData(res)
+        setFirstDay(getFirstDay(res.firstDay))
+        setWeekType(getWeekType(today))
       })
-      setWeekType(WeekType.ODD)
     }, []
   )
 
@@ -33,14 +47,48 @@ const Schedule: React.FC = () => {
     return json
   }
 
+  const getFirstDay = (day: string) => {
+    const dayArray = day.split('.')
+    return new Date(Date.parse(`${dayArray[1]}/${dayArray[0]}/${dayArray[2]}`))
+  }
+
+  const getWeekType = (date: Date) => {
+      const dateCopy = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+      // Set to nearest Thursday: current date + 4 - current day number
+      // Make Sunday's day number 7
+      dateCopy.setUTCDate(dateCopy.getUTCDate() + 4 - (dateCopy.getUTCDay()||7));
+      // Calculate full weeks to nearest Thursday
+      const weekNumber: number = Math.ceil((((dateCopy.valueOf() - firstDay.valueOf()) / 86400000) + 1) / 7);
+      // Return week number
+      return weekNumber % 2 === 0 ? WeekType.EVEN : WeekType.ODD;
+  }
+
+  const todayFormated = () => {
+    const year = today.getFullYear()
+    const month = (today.getMonth() + 1).toString().padStart(2, "0");
+    const day   = today.getDate().toString().padStart(2, "0");
+    return `${day}.${month}.${year}`
+  }
+
+  const changeWeekType = (e: any) => {
+    const text: string = e.target.innerText
+    const newWeekType = text === 'числитель' ? WeekType.EVEN : WeekType.ODD
+    setWeekType(newWeekType)
+  }
+
   return (
     <div className="schedule-container">
       <h2>Расписание</h2>
       <p>{data.name}</p>
+      <p className="today-container">{todayFormated()} - {getWeekType(today) === WeekType.EVEN ? 'числ.' : 'знам.'}</p>
+      <div className="switch-week-container">
+        <button className={weekType === WeekType.EVEN ? 'switch-weel-button-selected' : 'switch-weel-button-unselected'} onClick={changeWeekType}>числитель</button>
+        <button className={weekType === WeekType.ODD ? 'switch-weel-button-selected' : 'switch-weel-button-unselected'} onClick={changeWeekType}>знаменатель</button>
+      </div>
 
       <div className="row">
         {data.schedule.map((item) => (
-          <Card name={item.title} weekType={weekType}/>
+          <Card name={item.title} weekType={weekType} lectures={item.lectures}/>
         ))}
       </div>
     </div>
--- src/pages/Schedule/components/Card.tsx
+++ src/pages/Schedule/components/Card.tsx
@@ -1,19 +1,32 @@
 import "../../../style/Card.css"
 import React from "react";
-import { WeekType } from "../Schedule";
+import { Lecture, WeekType } from "../Schedule";
 
 interface CardConfig {
     name: string
     weekType: WeekType
+    lectures: Lecture[]
 }
 
 const Card: React.FC<CardConfig> = (props) => {
+
+  const compareWeekType = (lecture: Lecture, currentWeekType: WeekType) => {
+    const lectureWeekType = lecture.weekType === 'числитель' ? WeekType.EVEN : WeekType.ODD
+    return lectureWeekType === currentWeekType
+  }
+
   return (
     <div className="column">
         <div className="card">
         <h3>{props.name}</h3>
-        <p>Some text</p>
-        <p>Some text</p>
+
+        {props.lectures.filter(e => compareWeekType(e, props.weekType)).map((lecture) => (
+          <div className="inner-container">
+              <p className="leftcolumn">{lecture.time}</p>
+              <p className="middlecolumn">{lecture.name} {lecture.type === 'лекция' ? '(Лек)' : '(Пр)'}<br /><text className="lecturer-name">{lecture.lecturer}</text></p>
+              <p className="rightcolumn">{lecture.classroom}</p>
+          </div>
+        ))}
         </div>
     </div>
   )
